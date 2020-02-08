3cd425661adde8713b7fc8faf601d1aeea331ab7
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-02-08 00:46:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,4 +18,6 @@ function sayHiToGrandma(string) {
   if (string.toUpperCase() === string ){
     return "YES INDEED!"
   }
+  if (string === "I love you, Grandma")
+    return "I love you, too."
 }

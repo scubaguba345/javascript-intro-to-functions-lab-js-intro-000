d700aec2921f1b0a807a4fbd999d29145c63e47e
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-02-08 00:37:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function whisper(string) {
 function logShout(string){
   console.log (string.toUpperCase())
 }
-function logWhisper(strong)
+function logWhisper(strong){
   console.log (string.toLowerCase())
 }
